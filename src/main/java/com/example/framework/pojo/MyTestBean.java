dcd8c01972ad49b9316874c741dccc1bef9a815e
scaummb <201330290918a>
2021-01-04 01:16:36 +0800

    spring bean 创建demo

---
+++ src/main/java/com/example/framework/pojo/MyTestBean.java
@@ -0,0 +1,17 @@
+package com.example.framework.pojo;
+
+/**
+ * @author moubin.mo
+ * @date: 2021/1/3 08:57
+ */
+public class MyTestBean {
+	private String testStr = "testStr";
+
+	public String getTestStr() {
+		return testStr;
+	}
+
+	public void setTestStr(String testStr) {
+		this.testStr = testStr;
+	}
+}
---
+++ src/test/java/com/example/framework/BeanFactoryTest.java
@@ -0,0 +1,23 @@
+package com.example.framework;
+
+import com.example.framework.pojo.MyTestBean;
+import org.junit.Test;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author moubin.mo
+ * @date: 2021/1/3 08:59
+ */
+@SuppressWarnings("deprecation")
+public class BeanFactoryTest {
+	@Test
+	public void testSimpleLoad(){
+		BeanFactory bf = new XmlBeanFactory(new ClassPathResource("beanFactoryTest.xml"));
+		MyTestBean bean = (MyTestBean) bf.getBean("myTestBean");
+		assertEquals("testBean", bean.getTestStr());
+	}
+}
